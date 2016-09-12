0a883e42f7f7d29a0f2ed59b8a3facd50f3ccb1e
Jacob <kydd_jacob@wheatoncollege.edu>
2016-09-12 15:52:01 -0400

    removed vertical white space

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -12,21 +12,15 @@ using namespace chrono;
 int RandomWordGenorator()
 {
     ofstream outfile("randomwords.txt");
-
     srand (time(0));
     int numberofwords, lengthofwords, numberofletters = 0;
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
     string wordline;
-
     numberofwords = 10;
     lengthofwords = 10;
-
     string wordarray[numberofwords];
-
     steady_clock timer;
     auto start = timer.now();
-
-
     for (int j = 0; j < numberofwords; j++)
     {
         wordline = "";
@@ -37,17 +31,13 @@ int RandomWordGenorator()
         }
         wordarray[j] = wordline;
     }
-
     sort(wordarray, wordarray + sizeof wordarray / sizeof wordarray[0]);
     for (int j = 0; j < numberofwords; j++)
     {
         outfile << wordarray[j] << endl;
     }
-
     outfile.close();
-
     auto end = timer.now();
-
     duration<double> elapsedseconds = end-start;
     cout << "elapsed time: " << elapsedseconds.count() << "s\n";
 }
