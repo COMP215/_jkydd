80ddc4a1639d28330c56750564ac88e9d67da341
Jacob <kydd_jacob@wheatoncollege.edu>
2016-12-14 17:50:10 -0500

    finished final project

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <array>
 
 using namespace std;
 
@@ -9,25 +9,22 @@ using namespace std;
 int main()
 {
     //input
-    int month , day , year;
+    int month, day, year;
     cout << "Enter a date MM/DD/YYYY" << endl;
     cout << "Month:";
-    //cin >> month;
-    month = 9;
+    cin >> month;
+    //month = 9;
     cout << "Day:";
-    //cin >> day;
-    day = 7;
+    cin >> day;
+    //day = 7;
     cout << "Year:";
-    //cin >> year;
-    year = 1978;
+    cin >> year;
+    //year = 1978;
     //find doomsday of the year
     int cal1, cal2, cal3, cal4, total, doomsday;
     cal1 = (year%100)/12;
     cal2 = (year%100)-(cal1*12);
-    cout << cal1;
-    cout << cal2;
     cal3 = cal2/4;
-    cout << cal3;
     /*centuries anchor day
     1800 - 1899: Friday or 5
     1900 - 1999: Wednesday or 3
@@ -45,8 +42,20 @@ int main()
     if (2100 < year && year < 2199){
         cal4 = 0;
     }
-    cout << cal4;
     total = cal1+cal2+cal3+cal4;
     doomsday = total%7;
-    cout << "doomsday" << doomsday << endl;
+    array <string,7> days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    //the doomsday of the year
+    cout << "Doomsday:" << days[doomsday] << endl;
+
+    //now find the dates day of the week in relation to the dooms day
+    array <int,12> monthDoomsdays = {3,7,7,4,2,6,4,1,5,3,7,5};
+    array <int,12> monthDoomsdaysLeap = {4,1,7,4,2,6,4,1,5,3,7,5};
+    int finalday;
+    if (year%4 == 0 && year%100 == 0){
+        finalday = (doomsday+((day - monthDoomsdaysLeap[month-1])%7))%7;
+    }else{
+        finalday = (doomsday+((day - monthDoomsdays[month-1])%7))%7;
+    }
+    cout << "that date is a " << days[finalday] << endl;
 }
